a75bb3975242ec490afbe85e264d10cba58ade52
Your Name <communication@bridge2food.com>
2024-07-04 09:37:43 +0200

    initial content and layout

---
+++ art.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(plotly)
+library(svglite)
+
+# From FAOSTAT food balance sheets. Data for year 2021.
+# Animal and vegetal protein g/capita/day and population for all countries.
+art_data <- read_csv("data_raw/world.csv")
+
+art_data_wide <- art_data %>%
+  pivot_wider(id_cols = c(Area, Year), values_from = Value, names_from = Item)
+
+home_art <- art_data_wide %>%
+  ggplot(
+    aes(`Animal Products`, `Vegetal Products`, color = Area, size = Population)
+  ) +
+  geom_point(show.legend = F, alpha = 0.5, stroke = 0) +
+  scale_x_reverse() +
+  scale_y_reverse() +
+  theme_void() +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    #panel.border = element_blank(),
+    #panel.grid = element_blank(),
+    #axis.ticks = element_blank(),
+    #axis.ticks.length = unit(0, "cm"),
+    #axis.text.x = element_blank(),
+    #axis.text.y = element_blank()
+  )
+
+ggsave("home_art.svg", plot = home_art, device = "svg", bg = "transparent")
+  
